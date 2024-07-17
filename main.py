d280db251456e8c4b1cb008a1c1d34f780545b7b
Mogamad Imtiyaaz Hartley <imtihart@gmail.com>
2024-07-17 16:53:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+from replit import clear
+from art import logo
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    on = True
+    for on:
+        
+
+
